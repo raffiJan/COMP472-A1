52e79cf09cc4839bc45e99c1c381846f5c0dbfda
GaroBal <45246707+GaroBal@users.noreply.github.com>
2020-10-19 23:54:42 -0400

    Changes to Perceptron Models

--- PerceptronDS1.py
+++ PerceptronDS1.py
@@ -2,15 +2,15 @@ import pandas
 import numpy
 import csv
 from sklearn.linear_model import Perceptron
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 
 # Initialize variables for Dataset 1
 
-infoData1 = pandas.read_csv("Assig1-Dataset/info_1.csv", header=None).to_numpy()
-testNLData1 = pandas.read_csv("Assig1-Dataset/test_no_label_1.csv", header=None).to_numpy()
-testWLData1 = pandas.read_csv("Assig1-Dataset/test_with_label_1.csv", header=None).to_numpy()
-trainData1 = pandas.read_csv("Assig1-Dataset/train_1.csv", header=None).to_numpy()
-valData1 = pandas.read_csv("Assig1-Dataset/val_1.csv", header=None).to_numpy()
+infoData1 = pandas.read_csv("dataset/info_1.csv", header=None).to_numpy()
+testNLData1 = pandas.read_csv("dataset/test_no_label_1.csv", header=None).to_numpy()
+testWLData1 = pandas.read_csv("dataset/test_with_label_1.csv", header=None).to_numpy()
+trainData1 = pandas.read_csv("dataset/train_1.csv", header=None).to_numpy()
+valData1 = pandas.read_csv("dataset/val_1.csv", header=None).to_numpy()
 
 trainData1X = trainData1[:,:-1]
 trainData1Y = trainData1[:,-1]
@@ -24,29 +24,29 @@ per.fit(trainData1X, trainData1Y)
 predictPer1 = per.predict(testData1X)
 
 # Print Output to .csv
-
-##with open("Outputs/PER-DS1.csv", "a") as
-
 ## 1. Plot the distribution of the number of the instances in each class.
 
-pandas.DataFrame(numpy.bincount(trainData1Y)).to_csv("Outputs/PER-DS1.csv", mode = "a")
+pandas.DataFrame(numpy.bincount(trainData1Y)).to_csv("output/PER-DS1.csv", mode = "a", header = ["Distribution of class instances in Dataset"])
 
 ## a) Row number of instance, Index of predicted class of that instance
 
-pandas.DataFrame(predictPer1).to_csv("Outputs/PER-DS1.csv", mode = "a")
+pandas.DataFrame(predictPer1).to_csv("output/PER-DS1.csv", mode = "a", header =["Predicted Class"] )
 
 ## b) Plot the confusion matrix
 
-pandas.DataFrame(confusion_matrix(testData1Y, predictPer1)).to_csv("Outputs/PER-DS1.csv", mode = "a")
+pandas.DataFrame(confusion_matrix(testData1Y, predictPer1)).to_csv("output/PER-DS1.csv", mode = "a")
 
 ## c) The precision, recall, and f1-measure for each class
 
-pandas.DataFrame(precision_score(testData1Y, predictPer1, average = None)).to_csv("Outputs/PER-DS1.csv", mode = "a")
-pandas.DataFrame(recall_score(testData1Y, predictPer1, average = None)).to_csv("Outputs/PER-DS1.csv", mode = "a")
-pandas.DataFrame(f1_score(testData1Y, predictPer1, average = None)).to_csv("Outputs/PER-DS1.csv", mode = "a")
+pandas.DataFrame(precision_score(testData1Y, predictPer1, average = None)).to_csv("output/PER-DS1.csv", mode = "a", header = ["Precision Score"])
+pandas.DataFrame(recall_score(testData1Y, predictPer1, average = None)).to_csv("output/PER-DS1.csv", mode = "a", header = ["Recall Score"])
+pandas.DataFrame(f1_score(testData1Y, predictPer1, average = None)).to_csv("output/PER-DS1.csv", mode = "a", header = ["F-1 Measure"])
 
 ## d) The accuracy, macro-average f1 and weighted-average f1 of the model
 
-# pandas.DataFrame(accuracy_score(testData1Y, predictPer1).to_csv("Outputs/PER-DS1", mode = "a")
-# pandas.DataFrame(f1_score(testData1Y, predictPer1, average = "micro")).to_csv("Outputs/PER-DS1", mode = "a")
-# pandas.DataFrame(f1_score(testData1Y, predictPer1, average = "weighted")).to_csv("Outputs/PER-DS1", mode = "a")
+with open("output/PER-DS1.csv", "a") as file:
+    writefile = csv.writer(file)
+    writefile.writerow("")
+    writefile.writerow(["Accuracy: ", accuracy_score(testData1Y, predictPer1)])
+    writefile.writerow(["Macro Average F1: ", f1_score(testData1Y, predictPer1, average = "macro")])
+    writefile.writerow(["Weighted Average F1: ", f1_score(testData1Y, predictPer1, average = "weighted")])
--- PerceptronDS2.py
+++ PerceptronDS2.py
@@ -6,11 +6,11 @@ from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_
 
 # Initialize variables for Dataset 2
 
-infoData2 = pandas.read_csv("Assig1-Dataset/info_2.csv", header=None).to_numpy()
-testNLData2 = pandas.read_csv("Assig1-Dataset/test_no_label_2.csv", header=None).to_numpy()
-testWLData2 = pandas.read_csv("Assig1-Dataset/test_with_label_2.csv", header=None).to_numpy()
-trainData2 = pandas.read_csv("Assig1-Dataset/train_2.csv", header=None).to_numpy()
-valData2 = pandas.read_csv("Assig1-Dataset/val_2.csv", header=None).to_numpy()
+infoData2 = pandas.read_csv("dataset/info_2.csv", header=None).to_numpy()
+testNLData2 = pandas.read_csv("dataset/test_no_label_2.csv", header=None).to_numpy()
+testWLData2 = pandas.read_csv("dataset/test_with_label_2.csv", header=None).to_numpy()
+trainData2 = pandas.read_csv("dataset/train_2.csv", header=None).to_numpy()
+valData2 = pandas.read_csv("dataset/val_2.csv", header=None).to_numpy()
 
 trainData2X = trainData2[:,:-1]
 trainData2Y = trainData2[:,-1]
@@ -24,29 +24,29 @@ per.fit(trainData2X, trainData2Y)
 predictPer2 = per.predict(testData2X)
 
 # Print Output to .csv
-
-#with open("Outputs/PER-DS2.csv", "a") as
-
 ## 1. Plot the distribution of the number of the instances in each class.
 
-pandas.DataFrame(numpy.bincount(trainData2Y)).to_csv("Outputs/PER-DS2.csv", mode = "a")
+pandas.DataFrame(numpy.bincount(trainData2Y)).to_csv("output/PER-DS2.csv", mode = "a", header = ["Distribution of class instances in Dataset"])
 
 ## a) Row number of instance, Index of predicted class of that instance
 
-pandas.DataFrame(predictPer2).to_csv("Outputs/PER-DS2.csv", mode = "a")
+pandas.DataFrame(predictPer2).to_csv("output/PER-DS2.csv", mode = "a", header =["Predicted Class"] )
 
 ## b) Plot the confusion matrix
 
-pandas.DataFrame(confusion_matrix(testData2Y, predictPer2)).to_csv("Outputs/PER-DS2.csv", mode = "a")
+pandas.DataFrame(confusion_matrix(testData2Y, predictPer2)).to_csv("output/PER-DS2.csv", mode = "a")
 
 ## c) The precision, recall, and f1-measure for each class
 
-pandas.DataFrame(precision_score(testData2Y, predictPer2, average = None)).to_csv("Outputs/PER-DS2.csv", mode = "a")
-pandas.DataFrame(recall_score(testData2Y, predictPer2, average = None)).to_csv("Outputs/PER-DS2.csv", mode = "a")
-pandas.DataFrame(f1_score(testData2Y, predictPer2, average = None)).to_csv("Outputs/PER-DS2.csv", mode = "a")
+pandas.DataFrame(precision_score(testData2Y, predictPer2, average = None)).to_csv("output/PER-DS2.csv", mode = "a", header = ["Precision Score"])
+pandas.DataFrame(recall_score(testData2Y, predictPer2, average = None)).to_csv("output/PER-DS2.csv", mode = "a", header = ["Recall Score"])
+pandas.DataFrame(f1_score(testData2Y, predictPer2, average = None)).to_csv("output/PER-DS2.csv", mode = "a", header = ["F-1 Measure"])
 
 ## d) The accuracy, macro-average f1 and weighted-average f1 of the model
 
-# pandas.DataFrame(accuracy_score(testData1Y, predictPer1).to_csv("Outputs/PER-DS1", mode = "a")
-# pandas.DataFrame(f1_score(testData1Y, predictPer1, average = "micro")).to_csv("Outputs/PER-DS1", mode = "a")
-# pandas.DataFrame(f1_score(testData1Y, predictPer1, average = "weighted")).to_csv("Outputs/PER-DS1", mode = "a")
+with open("output/PER-DS2.csv", "a") as file:
+    writefile = csv.writer(file)
+    writefile.writerow("")
+    writefile.writerow(["Accuracy: ", accuracy_score(testData2Y, predictPer2)])
+    writefile.writerow(["Macro Average F1: ", f1_score(testData2Y, predictPer2, average = "macro")])
+    writefile.writerow(["Weighted Average F1: ", f1_score(testData2Y, predictPer2, average = "weighted")])
